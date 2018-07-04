67d23ef24a297998f38c6099aa01991fdc273777
Rahul <rahul.developer@gmail.com>
2018-07-05 00:08:50 +0530

    Added Sample

---
+++ TryOut/Part 2/Docker.Tryout/Docker.Tryout/Controllers/TryoutController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Docker.Tryout.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TryoutController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> HeartBeat()
+        {
+            return new string[] { "Beat 1", "Beat 2" };
+        }
+    }
+}
---
+++ TryOut/Part 2/Docker.Tryout/Docker.Tryout/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Docker.Tryout
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                            .AddEnvironmentVariables()
+                            .Build();
+
+
+            return
+            WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
+                .UseConfiguration(config)
+                .UseStartup<Startup>();
+        }
+    }
+}
